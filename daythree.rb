e6a2334da26562ad623f79f6c592a4b1a5d70b97
HanWax <hannahwaxman@Hannahs-MacBook-Pro.local>
2014-06-25 11:25:47 +0100

    Redoing examples from Day 3

---
+++ daythree.rb
@@ -0,0 +1,38 @@
+def print_header
+  print "The students of my cohort at Makers Academy\n"
+  print "-------------\n"
+end
+
+def print_names(students)
+  students.each_with_index do |student, index|
+    print "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort) \n"
+  end
+end
+
+def print_footer(names)    
+  print "Overall, we have #{names.length} great students \n"
+end
+
+def input_students
+  print "Please enter the names of the students \n"
+  print "To finish, just hit return twice \n"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do    
+    # add the student hash to the array
+    students << {:name => name, :cohort => :november}    
+    print "Now we have #{students.length} students\n"
+    # get another name from the user
+    name = gets.chomp
+  end
+  # return the array of students
+  students
+end
+
+students = input_students
+print_header
+print_names(students)
+print_footer(students)
